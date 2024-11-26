1271b46fc7cbe6fcda285c3dbece8aafa4340762
hyosoek <tennfin1@gmail.com>
2024-11-26 11:53:43 +0900

    init

---
+++ djangoECS/views.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+@api_view(['GET'])
+def index(request):
+    data = {"result": "success",
+            "data": [{"id":"itstudy", "name":"군계"},
+                     {"id":"ggangpae1", "name":"아담"}]}
+    return Response(data, status=status.HTTP_200_OK)
+
+# =>urls.py 파일에 요청 처리 코드를 추가
+
