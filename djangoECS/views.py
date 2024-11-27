ac1333702fea0bd87a7c313b022c064b1904c0cc
hyosoek <tennfin1@gmail.com>
2024-11-27 10:56:08 +0900

    fix(views.py) : add data

--- djangoECS/views.py
+++ djangoECS/views.py
@@ -6,7 +6,8 @@ from rest_framework import status
 def index(request):
     data = {"result": "success",
             "data": [{"id":"itstudy", "name":"군계"},
-                     {"id":"ggangpae1", "name":"아담"}]}
+                     {"id":"ggangpae1", "name":"아담"},
+                     {"id":"tennfin1", "name":"효석"}]}
     return Response(data, status=status.HTTP_200_OK)
 
 # =>urls.py 파일에 요청 처리 코드를 추가
